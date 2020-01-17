f76181fd4c2c888226f6a2ce589a7ca6152ce6b5
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-17 22:22:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -17,7 +17,7 @@ def find_max_value(array1 = [11,2,3,1,12,13,5,9,10,50,45])
   return array1[-1]
 end
 
-def find_min_value(array)
+def find_min_value(array1 = [11,2,3,1,12,13,5,9,10,50,45])
  array1.sort!
   return array1[0]
 end
