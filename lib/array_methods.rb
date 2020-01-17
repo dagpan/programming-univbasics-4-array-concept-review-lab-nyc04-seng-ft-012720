e9fc22c976e2f6396e7aaaaf4e94f5ed82168884
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-17 21:58:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -21,10 +21,11 @@ def find_element_index(array = [11,2,3,1,12,13], value = 11)
   while array[index] do
        if array[index] == value
           return index
+          index += 1
        else
           return nil
+          index += 1
        end
-       index += 1
   end
 end
 
